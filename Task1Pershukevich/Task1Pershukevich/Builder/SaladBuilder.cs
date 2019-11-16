6932d3e173ef9817b4c1b281d036adbb1a985657
Vlad Pershukevich <mr.vlad0207@mail.ru>
2019-11-16 18:21:14 +0300

    Builder added

---
+++ Task1Pershukevich/Task1Pershukevich/Builder/SaladBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1Pershukevich.MainSalad;
+
+namespace Task1Pershukevich.Builder
+{
+    public class SaladBuilder : ISaladBuilder
+    {
+        public Salad salad { get; private set; }
+    }
+}
--- Task1Pershukevich/Task1Pershukevich/MainChief/Chief.cs
+++ Task1Pershukevich/Task1Pershukevich/MainChief/Chief.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.Text;
 using Task1Pershukevich.Ingridients;
 using Task1Pershukevich.MainSalad;
+using Task1Pershukevich.Builder;
 
 namespace Task1Pershukevich.MainChief
 {
     public class Chief : IChief
     {
-        private void AddIngridientToSalad(Ingridient ingridient)
+        private void AddIngridientToSalad(Ingridient ingridient) 
         {
             Console.WriteLine($"Adding {ingridient.Name} to salad");
         }
 
-        public void CheckEveryIngridient(Salad salad)
+        public void CheckEveryIngridient(Salad salad) //not correct
         {
             foreach(Ingridient i in salad.GetAllIngridients())
             {
                 AddIngridientToSalad(i);
             }
             Console.WriteLine($"Salad {salad.Name} is ready!");
+        }
+
+        public Salad MakeSalad(SaladBuilder builder) //fill
+        {
+            return builder.salad;
         }
     }
 }
