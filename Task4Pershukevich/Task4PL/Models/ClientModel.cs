62ddb1ee04ea839faf293821c2187ce63e5eef87
Vlad Pershukevich <mr.vlad0207@mail.ru>
2020-01-08 12:25:55 +0300

    models and controllers folder filled

---
+++ Task4Pershukevich/Task4PL/Models/ClientModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4PL.Models
+{
+    public class ClientModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ Task4Pershukevich/Task4PL/Models/ManagerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4PL.Models
+{
+    public class ManagerModel
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+    }
+}
