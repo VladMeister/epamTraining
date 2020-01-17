85df964d1b97c8632cf034e9464cf4a1520cad16
Vlad Pershukevich <mr.vlad0207@mail.ru>
2020-01-17 10:50:33 +0300

    models added
    packages installed

---
+++ Task5Pershukevich/Task5.WEB/Models/ClientViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task5.WEB.Models
+{
+    public class ClientViewModel
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ Task5Pershukevich/Task5.WEB/Models/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task5.WEB.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public double Cost { get; set; }
+        public DateTime Date { get; set; }
+
+        public int ManagerId { get; set; }
+        public int ProductId { get; set; }
+        public int ClientId { get; set; }
+    }
+}
